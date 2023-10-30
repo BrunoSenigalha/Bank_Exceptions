cdd1d128d800c31094123d9d86af3deb2acc7bb8
Bruno Senigalha <brsenigalha@gmail.com>
2023-10-30 14:18:42 -0300

    first commit

---
+++ Bank_Exceptions/Entities/Account.cs
@@ -0,0 +1,48 @@
+﻿using Bank_Exceptions.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Exceptions.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Amount can't be negative or iqual to zero.");
+            }
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+
+            if(amount > Balance)
+            {
+                throw new DomainException("Not enough balance");
+            }
+            Balance -= amount;
+        }
+    }
+}
---
+++ Bank_Exceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using Bank_Exceptions.Entities;
+using Bank_Exceptions.Entities.Exceptions;
+
+try
+{
+    Console.WriteLine("Enter account data");
+    Console.Write("Number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Holder: ");
+    string holder = Console.ReadLine();
+    Console.Write("Initial Balance: ");
+    double initialBalance = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Withdraw limit: ");
+    double withdrawLimit = Convert.ToDouble(Console.ReadLine());
+
+    Account account = new Account(number, holder, initialBalance, withdrawLimit);
+
+    Console.WriteLine();
+    Console.Write("Enter amount for withdraw: ");
+    double amount = Convert.ToDouble(Console.ReadLine());
+    account.Withdraw(amount);
+    Console.Write($"New Balance: {account.Balance.ToString("F2")}");
+}
+catch(DomainException e)
+{
+    Console.WriteLine("Withdraw error: " + e.Message);
+}
+catch(FormatException e)
+{
+    Console.WriteLine("Format error: " + e.Message);
+}
+catch (Exception)
+{
+    Console.WriteLine("Unknow error");
+}
